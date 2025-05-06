c7e2f219f371255e875ff15f583f0aaa8e738663
Anna Swetha R <144353019+r-swetha1473@users.noreply.github.com>
2025-05-06 16:27:59 +0530

    Update payment.service.ts

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -7,7 +7,7 @@ import { CartItem } from '../models/cart-item.model';
   providedIn: 'root'
 })
 export class PaymentService {
-  private baseUrl = 'http://34.121.150.114/api/payment'; // Backend URL
+  private baseUrl = 'https://34.121.150.114/api/payment'; // Backend URL
 
   constructor(private http: HttpClient) {}
 
